e8292aea8da8496576b519907c1489774e639af8
Aaron Kipf <samplethis84@gmail.com>
2026-03-03 15:09:19 -0600

    working on project

--- src/App.js
+++ src/App.js
@@ -1,22 +1,53 @@
 // `https://api.frankfurter.app/latest?amount=100&from=EUR&to=USD`
 
+import { useState, useEffect } from "react";
+
 export default function App() {
+  const [amount, setAmount] = useState(1);
+  const [fromCurrency, setFromCurrency] = useState("USD");
+  const [toCurrency, setToCurrency] = useState("EUR");
+  const [converted, setConverted] = useState("");
+
+  useEffect(function () {
+    async function convertCurrency() {
+      const res = await fetch(
+        `https://api.frankfurter.app/latest?amount=${amount}&from=${fromCurrency}&to=${toCurrency}`,
+      );
+      const data = await res.json();
+      setConverted(data.rates[toCurrency]);
+    }
+
+    convertCurrency();
+  }, []);
+
   return (
     <div>
-      <input type="text" />
-      <select>
+      <input
+        type="text"
+        value={amount}
+        onChange={(e) => setAmount(Number(e.target.value))}
+      />
+      <select
+        value={fromCurrency}
+        onChange={(e) => setFromCurrency(e.target.value)}
+      >
         <option value="USD">USD</option>
         <option value="EUR">EUR</option>
         <option value="CAD">CAD</option>
         <option value="INR">INR</option>
       </select>
-      <select>
+      <select
+        value={toCurrency}
+        onChange={(e) => setToCurrency(e.target.value)}
+      >
         <option value="USD">USD</option>
         <option value="EUR">EUR</option>
         <option value="CAD">CAD</option>
         <option value="INR">INR</option>
       </select>
-      <p>OUTPUT</p>
+      <p>
+        {converted} {toCurrency}
+      </p>
     </div>
   );
 }
